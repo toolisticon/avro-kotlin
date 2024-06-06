5d5d36d5dec3a02757ebbca1fedd4eedf34d761b
Jan Galinski <jan.galinski@holisticon.de>
2024-06-06 02:12:54 +0200

    feat: add tests

---
+++ _examples/kotlin-plugin-example/src/test/kotlin/SimpleUUIDRecordITest.kt
@@ -0,0 +1,19 @@
+package io.toolisticon.kotlin.avro.generator.test
+
+import io.toolisticon.avro.avro4k.schema.SimpleUUIDRecord
+import io.toolisticon.avro.avro4k.schema.SimpleUUIDRecordData
+import io.toolisticon.kotlin.avro.serialization.AvroKotlinSerialization
+import io.toolisticon.kotlin.avro.value.AvroFingerprint
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class SimpleUUIDRecordITest {
+
+  @Test
+  fun `schema of java and kotlin are identical`() {
+    val fingerprintJava = AvroFingerprint.of(SimpleUUIDRecord.getClassSchema())
+    val fingerprintKotlin = AvroKotlinSerialization().schema(SimpleUUIDRecordData::class).fingerprint
+
+    assertThat(fingerprintKotlin).isEqualTo(fingerprintJava)
+  }
+}
---
+++ _generator/avro-kotlin-generator/src/test/kotlin/processor/SuppressWarningsOnFileSpecProcessorTest.kt
@@ -0,0 +1,44 @@
+package io.toolisticon.kotlin.avro.generator.processor
+
+import io.toolisticon.kotlin.avro.generator.api.AvroKotlinGeneratorProperties
+import io.toolisticon.kotlin.generation.builder.KotlinFileBuilder
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+
+internal class SuppressWarningsOnFileSpecProcessorTest {
+
+  @Test
+  fun `add single suppression`() {
+    val processor = SuppressWarningsOnFileSpecProcessor(suppressions = listOf("Hello"))
+
+    val file = KotlinFileBuilder.builder("foo", "Bar")
+      .apply {
+        processor.addSuppressAnnotation(
+          AvroKotlinGeneratorProperties(suppressRedundantModifiers = true),
+          this
+        )
+      }.build()
+
+    assertThat(file.code.trim().lines().first()).isEqualTo(
+      """@file:Suppress("Hello")"""
+    )
+  }
+
+  @Test
+  fun `add multiple suppressions`() {
+    val processor = SuppressWarningsOnFileSpecProcessor(suppressions = listOf("Hello", "World"))
+
+    val file = KotlinFileBuilder.builder("foo", "Bar")
+      .apply {
+        processor.addSuppressAnnotation(
+          AvroKotlinGeneratorProperties(suppressRedundantModifiers = true),
+          this
+        )
+      }.build()
+
+    assertThat(file.code.trim().lines().first()).isEqualTo(
+      """@file:Suppress("Hello", "World")"""
+    )
+  }
+}
