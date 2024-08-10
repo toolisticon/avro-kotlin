ced547dbd987a7cfaeb4007f28379fa8c1e7e9bb
Jan Galinski <jan.galinski@holisticon.de>
2024-08-10 12:39:40 +0200

    chore: format & docs

--- _examples/logical-type-customer-id/src/main/kotlin/CustomerId.kt
+++ _examples/logical-type-customer-id/src/main/kotlin/CustomerId.kt
@@ -1,17 +1,18 @@
 package io.toolisticon.kotlin.avro.example.customerid
 
+import io.toolisticon.kotlin.avro.example.customerid.CustomerIdLogicalType.CustomerIdSerializer
 import kotlinx.serialization.Serializable
 import java.util.*
 
 @JvmInline
-@Serializable(with = CustomerIdLogicalType.CustomerIdSerializer::class)
+@Serializable(with = CustomerIdSerializer::class)
 value class CustomerId(val id: String) {
   companion object {
     @JvmStatic
     fun random(): CustomerId = of(UUID.randomUUID().toString())
 
     @JvmStatic
-    fun of(id: String):CustomerId = CustomerId(id)
+    fun of(id: String): CustomerId = CustomerId(id)
   }
 
   override fun toString() = id
