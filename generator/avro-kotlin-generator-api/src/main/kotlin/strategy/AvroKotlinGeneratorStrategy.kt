16bad5274818904fecb5190811547cc53c6970b5
Jan Galinski <jan.galinski@holisticon.de>
2024-08-25 00:57:15 +0200

    chore: refactor _build, fixes #116

--- generator/avro-kotlin-generator-api/src/main/kotlin/strategy/AvroKotlinGeneratorStrategy.kt
+++
@@ -1,9 +0,0 @@
-package io.toolisticon.kotlin.avro.generator.api.strategy
-
-/**
- * Lists all possible strategies, so the service loader in impl can provide them to api.
- */
-interface AvroKotlinGeneratorStrategies {
-  val generateDataClassStrategy : GenerateDataClassStrategy
-  val generateEnumClassStrategy: GenerateEnumClassStrategy
-}
