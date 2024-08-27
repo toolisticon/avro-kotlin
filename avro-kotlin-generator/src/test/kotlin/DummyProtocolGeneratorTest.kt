ec6ea3f8a6a4b7ed671c4eb11813919aa4b73086
Jan Galinski <jan.galinski@holisticon.de>
2024-08-27 23:59:39 +0200

    chore: prepare protocol test

---
+++ avro-kotlin-generator/src/test/kotlin/DummyProtocolGeneratorTest.kt
@@ -0,0 +1,16 @@
+package io.toolisticon.kotlin.avro.generator
+
+import _ktx.ResourceKtx.resourceUrl
+import io.toolisticon.kotlin.avro.declaration.ProtocolDeclaration
+import org.junit.jupiter.api.Test
+
+class DummyProtocolGeneratorTest {
+
+  private val generator = TestFixtures.DEFAULT_GENERATOR
+  private val declaration : ProtocolDeclaration = TestFixtures.PARSER.parseProtocol(resourceUrl("protocol/DummyProtocol.avpr"))
+
+  @Test
+  fun `generate dummy protocol`() {
+    println(declaration.originalJson)
+  }
+}
