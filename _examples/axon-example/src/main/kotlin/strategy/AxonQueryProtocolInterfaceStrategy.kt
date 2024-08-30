3ce21ba7477a8a104127f7b6ebaafcf8075f8d42
Simon Zambrovski <simon.zambrovski@holisticon.de>
2024-08-30 14:33:49 +0200

    ongoing

--- _examples/axon-example/src/main/kotlin/strategy/AxonQueryProtocolInterfaceStrategy.kt
+++ _examples/axon-example/src/main/kotlin/strategy/AxonQueryProtocolInterfaceStrategy.kt
@@ -15,29 +15,43 @@ import io.toolisticon.kotlin.avro.generator.strategy.AvroFileSpecFromProtocolDec
 import io.toolisticon.kotlin.avro.model.AvroType
 import io.toolisticon.kotlin.avro.model.RecordType
 import io.toolisticon.kotlin.avro.model.wrapper.AvroProtocol
+import io.toolisticon.kotlin.avro.value.Documentation
 import io.toolisticon.kotlin.avro.value.Name
 import io.toolisticon.kotlin.generation.KotlinCodeGeneration.buildFun
 import io.toolisticon.kotlin.generation.KotlinCodeGeneration.builder
+import io.toolisticon.kotlin.generation.KotlinCodeGeneration.builder.objectBuilder
 import io.toolisticon.kotlin.generation.spec.KotlinFileSpec
 import io.toolisticon.kotlin.generation.support.GeneratedAnnotation
 
 @OptIn(ExperimentalKotlinPoetApi::class)
-class AxonQueryProtocolStrategy : AvroFileSpecFromProtocolDeclarationStrategy() {
+class AxonQueryProtocolInterfaceStrategy : AvroFileSpecFromProtocolDeclarationStrategy() {
 
   override fun invoke(context: ProtocolDeclarationContext, input: ProtocolDeclaration): KotlinFileSpec {
 
     val fileName: ClassName = (input.canonicalName.namespace + Name(input.name.value + "QueryProtocol")).asClassName()
     val fileBuilder = builder.fileBuilder(fileName)
 
+    val objectBuilder = objectBuilder(fileName).apply {
+      addAnnotation(GeneratedAnnotation(value = AvroKotlinGenerator.NAME))
+      addKDoc(input.documentation)
+    }
 
+    val allQueriesInterfaceName = (input.canonicalName.namespace + Name(input.name.value + "AllQueries")).asClassName()
+    val allQueriesInterfaceBuilder = builder.interfaceBuilder(allQueriesInterfaceName).apply {
+      addKDoc(Documentation("Union interface for all queries"))
+    }
 
-    input.protocol.messages.filterValues { message -> message.isQuery() }.forEach { (name, message) ->
+    /*
+    Single interface for each query
+     */
+    input.protocol.messages
+      .filterValues { message -> message.isQuery() }
+      .map { (name, message) ->
 
-      val queryInterfaceName = (input.canonicalName.namespace + Name(input.name.value + name.value.firstUppercase())).asClassName()
-
+      val queryInterfaceName = (input.canonicalName.namespace + Name(name.value.firstUppercase())).asClassName()
       val interfaceBuilder = builder.interfaceBuilder(queryInterfaceName).apply {
+        // TODO: the strategy should be a fall-through in order: on message, on message type, on referenced-type
         addKDoc(message.documentation)
-        addAnnotation(GeneratedAnnotation(value = AvroKotlinGenerator.NAME))
       }
 
       val function = buildFun(name.value) {
@@ -47,14 +61,19 @@ class AxonQueryProtocolStrategy : AvroFileSpecFromProtocolDeclarationStrategy() 
         }
       }
 
-      interfaceBuilder.addFunction(function)
-
-      fileBuilder.addType(
-        interfaceBuilder
-      )
+      interfaceBuilder.addFunction(function) // add function to the interface
+    }.forEach { interfaceBuilder ->
+        objectBuilder.addType(interfaceBuilder) // add interface to object
+        allQueriesInterfaceBuilder.addSuperinterface(ClassName.bestGuess(interfaceBuilder.spec().className.simpleName))
     }
 
+
+    objectBuilder.addType(allQueriesInterfaceBuilder) // add union interface
+    fileBuilder.addType(objectBuilder)
+
+
     // TODO run processors
+
     return fileBuilder.build()
   }
 
@@ -62,14 +81,4 @@ class AxonQueryProtocolStrategy : AvroFileSpecFromProtocolDeclarationStrategy() 
     return super.test(context, input) && input is ProtocolDeclaration && input.protocol.messages.values.any { message -> message.isQuery() }
   }
 
-
-  fun AvroProtocol.Message.isQuery(): Boolean {
-    return this.request.fields
-      .map { AvroType.avroType<AvroType>(it.schema) }
-      .filterIsInstance<RecordType>()
-      .mapNotNull { it.recordMetaData() }
-      .any { RecordMetaDataType.Query == it.type }
-    // TODO: analyze response of query
-    // RecordMetaDataType.QueryResult == it.type
-  }
 }
