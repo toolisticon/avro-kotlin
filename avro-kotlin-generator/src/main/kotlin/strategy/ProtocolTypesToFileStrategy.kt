3cd5a6a7b3f7fecc1e83f2291c916030b65b3f0a
Jan Galinski <jan.galinski@holisticon.de>
2024-09-28 22:55:50 +0200

    file generator, fixes #151

--- avro-kotlin-generator/src/main/kotlin/strategy/ProtocolTypesToFileStrategy.kt
+++ avro-kotlin-generator/src/main/kotlin/strategy/ProtocolTypesToFileStrategy.kt
@@ -4,27 +4,56 @@ import com.squareup.kotlinpoet.ExperimentalKotlinPoetApi
 import com.squareup.kotlinpoet.TypeName
 import io.toolisticon.kotlin.avro.declaration.ProtocolDeclaration
 import io.toolisticon.kotlin.avro.generator.AvroKotlinGenerator
+import io.toolisticon.kotlin.avro.generator.processor.ProtocolFileSpecProcessor
 import io.toolisticon.kotlin.avro.generator.spi.ProtocolDeclarationContext
 import io.toolisticon.kotlin.avro.generator.spi.ProtocolDeclarationContext.Companion.toSchemaDeclarationContext
-import io.toolisticon.kotlin.avro.generator.strategy.SchemaTypesToFileStrategy.Companion.logger
 import io.toolisticon.kotlin.avro.generator.strategy.internal.KotlinErrorTypeStrategy
 import io.toolisticon.kotlin.avro.model.*
 import io.toolisticon.kotlin.avro.value.toKebabCase
+import io.toolisticon.kotlin.generation.FileName
 import io.toolisticon.kotlin.generation.KotlinCodeGeneration.builder.fileBuilder
 import io.toolisticon.kotlin.generation.KotlinCodeGeneration.className
 import io.toolisticon.kotlin.generation.spec.KotlinFileSpec
 import io.toolisticon.kotlin.generation.spec.KotlinGeneratorTypeSpec
 import io.toolisticon.kotlin.generation.spi.strategy.executeAll
 import io.toolisticon.kotlin.generation.support.GeneratedAnnotation
+import mu.KLogging
 
 @OptIn(ExperimentalKotlinPoetApi::class)
-class ProtocolTypesToFileStrategy : AvroFileSpecFromProtocolDeclarationStrategy() {
-  override fun invoke(context: ProtocolDeclarationContext, input: ProtocolDeclaration): KotlinFileSpec {
-    val protocol = input.protocol
-    val kebabCase = protocol.name.toKebabCase()
+class ProtocolTypesToFileStrategy(
+  private val determineFileName: DetermineFileName,
+  private val avroNamedTypeFilter: AvroNamedTypeFilter,
+) : AvroFileSpecFromProtocolDeclarationStrategy() {
+  companion object : KLogging() {
 
+    /**
+     * Gives the name of the generated file.
+     */
+    fun interface DetermineFileName : (ProtocolDeclarationContext, ProtocolDeclaration) -> FileName
+
+    /**
+     * Returns `true` if the type should be considered for generation.
+     */
+    fun interface AvroNamedTypeFilter : (AvroNamedType) -> Boolean
+
+    val defaultDetermineFileName = DetermineFileName { context, input ->
+      val protocol = input.protocol
+      val kebabCase = protocol.name.toKebabCase()
+      className(input.canonicalName.namespace.value, "$kebabCase-types")
+    }
+
+    val defaultAvroNamedTypeFilter = AvroNamedTypeFilter { true }
+  }
+
+  /**
+   * Default constructor used by spi.
+   */
+  @Suppress("unused")
+  constructor() : this(determineFileName = defaultDetermineFileName, avroNamedTypeFilter = defaultAvroNamedTypeFilter)
+
+  override fun invoke(context: ProtocolDeclarationContext, input: ProtocolDeclaration): KotlinFileSpec {
     val fileBuilder = fileBuilder(
-      className = className(input.canonicalName.namespace.value, "$kebabCase-types")
+      className = determineFileName(context, input)
     ).apply {
       addAnnotation(GeneratedAnnotation(AvroKotlinGenerator.NAME))
     }
@@ -35,21 +64,22 @@ class ProtocolTypesToFileStrategy : AvroFileSpecFromProtocolDeclarationStrategy(
       it.avroType is AvroNamedType
     }.distinctBy { it.avroType.hashCode }.map { it.avroType as AvroNamedType to it.typeName }
 
-
-    val typeSpecs: List<KotlinGeneratorTypeSpec<*>> = subTypesToGenerate.flatMap<Pair<AvroNamedType, TypeName>, KotlinGeneratorTypeSpec<*>> { (type, _) ->
-      when (type) {
-        is RecordType -> schemaDeclarationContext.dataClassStrategies.executeAll(schemaDeclarationContext, type)
-        is EnumType -> schemaDeclarationContext.enumClassStrategies.executeAll(schemaDeclarationContext, type)
-        is ErrorType -> listOf(KotlinErrorTypeStrategy.execute(schemaDeclarationContext, type)).filterNotNull()
-        is FixedType, is RequestType -> {
-          logger.debug { "ignoring ${type::class.simpleName} type: $type.canonicalName" }
-          emptyList()
+    val typeSpecs: List<KotlinGeneratorTypeSpec<*>> = subTypesToGenerate.filter { avroNamedTypeFilter(it.first) }
+      .flatMap<Pair<AvroNamedType, TypeName>, KotlinGeneratorTypeSpec<*>> { (type, _) ->
+        when (type) {
+          is RecordType -> schemaDeclarationContext.dataClassStrategies.executeAll(schemaDeclarationContext, type)
+          is EnumType -> schemaDeclarationContext.enumClassStrategies.executeAll(schemaDeclarationContext, type)
+          is ErrorType -> listOf(KotlinErrorTypeStrategy.execute(schemaDeclarationContext, type)).filterNotNull()
+          is FixedType, is RequestType -> {
+            logger.debug { "ignoring ${type::class.simpleName} type: $type.canonicalName" }
+            emptyList()
+          }
         }
       }
-    }
 
     typeSpecs.forEach(fileBuilder::addType)
 
+    context.registry.processors.filter(ProtocolFileSpecProcessor::class).forEach { it.execute(context, fileBuilder) }
     return fileBuilder.build()
   }
 }
